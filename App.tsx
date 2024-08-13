5cdffd80278f83b5ce8167e6a56b0a54ca41a49f
Marcos Colella <colella.dev@gmail.com>
2024-08-13 15:42:15 -0300

    Componente Loading

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
-import { StatusBar, View } from 'react-native'
+import { StatusBar } from 'react-native'
+
 import {
   useFonts,
   Roboto_700Bold,
   Roboto_400Regular,
 } from '@expo-google-fonts/roboto'
+
 import { Center, GluestackUIProvider, Text } from '@gluestack-ui/themed'
 import { config } from './config/gluestack-ui.config'
+import { Loading } from '@components/Loading'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular })
@@ -23,7 +26,7 @@ export default function App() {
           <Text>Home</Text>
         </Center>
       ) : (
-        <View />
+        <Loading />
       )}
     </GluestackUIProvider>
   )
