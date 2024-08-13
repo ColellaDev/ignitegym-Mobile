7fd99e08292db70c4878b82be4694581f57389d9
Marcos Colella <colella.dev@gmail.com>
2024-08-13 16:11:55 -0300

    Tela Login e Imagem Background

--- App.tsx
+++ App.tsx
@@ -6,9 +6,10 @@ import {
   Roboto_400Regular,
 } from '@expo-google-fonts/roboto'
 
-import { Center, GluestackUIProvider, Text } from '@gluestack-ui/themed'
+import { GluestackUIProvider } from '@gluestack-ui/themed'
 import { config } from './config/gluestack-ui.config'
 import { Loading } from '@components/Loading'
+import { SignIn } from '@screens/SignIn'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular })
@@ -21,13 +22,7 @@ export default function App() {
         translucent
       />
 
-      {fontsLoaded ? (
-        <Center flex={1} bg="$gray700">
-          <Text>Home</Text>
-        </Center>
-      ) : (
-        <Loading />
-      )}
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </GluestackUIProvider>
   )
 }
