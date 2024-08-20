61447de1f31da8cf16b9ab44ea35e1c4ffc6a0db
Marcos Colella <colella.dev@gmail.com>
2024-08-20 20:32:06 -0300

    Exibindo Grupo Selecionado

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,14 +1,28 @@
-import { VStack} from '@gluestack-ui/themed'
+import { HStack, VStack} from '@gluestack-ui/themed'
+import { useState } from 'react'
 
 import { HomeHeader } from '@components/HomeHeader'
 import { Group } from '@components/Group'
 
 export function Home() {
+  const [groupSelected, setGroupSelected] = useState('costas')
+
   return (
     <VStack flex={1}>
       <HomeHeader/>
 
-      <Group name="Costas" isActive={false} />
+      <Group
+          name="Costas"
+          isActive={groupSelected === 'costas'}
+          onPress={() => setGroupSelected('costas')}
+        />
+
+        <Group
+        name="Ombro"
+        isActive={groupSelected === "ombro"}
+        onPress={()=> setGroupSelected("ombro")}
+        />
+
     </VStack>
   )
 }
