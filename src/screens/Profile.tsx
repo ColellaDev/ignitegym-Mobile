0e69243b78b6706ad4bb4588201e17cab41ce8e0
Marcos Colella <colella.dev@gmail.com>
2024-08-21 18:23:53 -0300

    Revertendo Mudança

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -9,7 +9,7 @@ import { Button } from '@components/Button'
 export function Profile() {
   return (
     <VStack flex={1}>
-      <ScreenHeader title="Bards"/>
+      <ScreenHeader title="Perfil"/>
 
       <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt="$6" px="$10">
