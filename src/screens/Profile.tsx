891f3542d2fcfac250c77d09e465d18df4ce1723
Marcos Colella <colella.dev@gmail.com>
2024-08-21 15:54:03 -0300

    Inputs de Nome e E-mails

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -2,6 +2,7 @@ import { UserPhoto } from '@components/UserPhoto'
 import { Center, VStack, Text } from '@gluestack-ui/themed'
 import { ScrollView, TouchableOpacity } from 'react-native'
 import { ScreenHeader } from '@components/ScreenHeader'
+import { Input } from '@components/Input'
 
 
 export function Profile() {
@@ -28,6 +29,11 @@ export function Profile() {
               Alterar Foto
             </Text>
           </TouchableOpacity>
+
+          <Center w="$full" gap="$4">
+            <Input placeholder="Nome" bg="$gray600" />
+            <Input value="marcos@email.com" bg="$gray600" isReadOnly />
+          </Center>
 
         </Center>
       </ScrollView>
