47a917022b417405966074f01d953491c7395e39
Marcos Colella <colella.dev@gmail.com>
2024-08-21 11:24:07 -0300

    Componente HistoryCard

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,10 +1,12 @@
 import { ScreenHeader } from '@components/ScreenHeader'
 import { VStack } from '@gluestack-ui/themed'
+import { HistoryCard } from '@components/HistoryCard'
 
 export function History() {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Histórico" />
+      <HistoryCard />
     </VStack>
   )
 }
