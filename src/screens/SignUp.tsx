812bc05dccc17ab487d8bdcd8eee9ad48e1f7dc7
Marcos Colella <colella.dev@gmail.com>
2024-09-02 18:34:19 -0300

    Tipando os dados do formulário

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -16,18 +16,25 @@ import Logo from '@assets/logo.svg'
 import { Input } from '@components/Input'
 import { Button } from '@components/Button'
 
+type FormDataProps = {
+  name: string;
+  email: string;
+  password: string;
+  password_confirm: string;
+}
+
 export function SignUp() {
 
   const navigation = useNavigation<AuthNavigatorRoutesProps>()
 
-  const { control, handleSubmit } = useForm();
+  const { control, handleSubmit } = useForm<FormDataProps>();
 
   function handleGoBack(){
     navigation.goBack()
   }
 
-  function handleSignUp(data: any) {
-    console.log({ data })
+  function handleSignUp({ name, email, password, password_confirm }: FormDataProps) {
+    console.log({ name, email, password, password_confirm })
   }
 
   return (
