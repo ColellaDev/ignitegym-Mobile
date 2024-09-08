73ed9e04be0f84670bfb0608b817978bde11a926
Marcos Colella <colella.dev@gmail.com>
2024-09-08 14:09:05 -0300

    Formulário SignIn

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,73 +1,102 @@
-import {
-  Center,
-  Heading,
-  Image,
-  ScrollView,
-  Text,
-  VStack,
-} from '@gluestack-ui/themed'
+import { Controller, useForm } from 'react-hook-form';
+import { useNavigation } from "@react-navigation/native";
+import { VStack, Image, Text, Center, Heading, ScrollView } from '@gluestack-ui/themed';
 
-import { useNavigation } from '@react-navigation/native'
-import { AuthNavigatorRoutesProps } from "@routes/auth.routes"
+import { AuthNavigatorRoutesProps } from '@routes/auth.routes';
 
-import BackgroundImg from '@assets/background.png'
-import Logo from '@assets/logo.svg'
-import { Input } from '@components/Input'
-import { Button } from '@components/Button'
+import LogoSvg from '@assets/logo.svg';
+import BackgroundImg from '@assets/background.png';
+
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
+
+type FormData = {
+  email: string;
+  password: string;
+}
 
 export function SignIn() {
 
-  const navigation = useNavigation<AuthNavigatorRoutesProps>()
+  const navigation = useNavigation<AuthNavigatorRoutesProps>();
+
+  const { control, handleSubmit, formState: { errors } } = useForm<FormData>()
 
   function handleNewAccount() {
-    navigation.navigate("signUp")
+    navigation.navigate('signUp');
   }
+
+  function handleSignIn({ email, password }: FormData){
+    console.log(email, password)
+  }
+
   return (
-    <ScrollView
-      contentContainerStyle={{ flexGrow: 1 }}
-      showsVerticalScrollIndicator={false}
-    >
-      <VStack flex={1}>
-        <Image
-          w="$full"
-          h={624}
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
+        <VStack flex={1} px="$10" pb="$16">
+        <Image 
           source={BackgroundImg}
           defaultSource={BackgroundImg}
           alt="Pessoas treinando"
+          resizeMode="contain"
           position="absolute"
         />
 
-        <VStack flex={1} px="$10" pb="$16">
-          <Center my="$24">
-            <Logo />
+        <Center my="$24">
+          <LogoSvg />
 
-            <Text color="$gray100" fontSize="$sm">
-              Treine sua mente e seu corpo
-            </Text>
-          </Center>
+          <Text color="$gray100" fontSize="$sm">
+            Treine sua mente e o seu corpo.
+          </Text>
+        </Center>
 
-          <Center gap="$2">
-            <Heading color="$gray100">Acesse sua conta</Heading>
+        <Center>
+          <Heading color="$gray100" fontSize="$xl" mb="$6" fontFamily="$heading">
+            Acesse a conta
+          </Heading>
 
-            <Input
-              placeholder="E-mail"
-              keyboardType="email-address"
-              autoCapitalize="none"
-            />
-            <Input placeholder="Senha" secureTextEntry />
+          <Controller 
+            control={control}
+            name="email"
+            rules={{ required: 'Informe o e-mail' }}
+            render={({ field: { onChange } }) => (
+              <Input 
+                placeholder="E-mail" 
+                keyboardType="email-address"
+                autoCapitalize="none"
+                onChangeText={onChange}
+                errorMessage={errors.email?.message}
+              />
+            )}
+          />
+          
+          <Controller 
+            control={control}
+            name="password"
+            rules={{ required: 'Informe a senha' }}
+            render={({ field: { onChange } }) => (
+              <Input 
+                placeholder="Senha" 
+                secureTextEntry
+                onChangeText={onChange}
+                errorMessage={errors.password?.message}
+              />
+            )}
+          />
 
-            <Button title="Acessar" />
-          </Center>
+          <Button title="Acessar" onPress={handleSubmit(handleSignIn)} />
+        </Center>
 
-          <Center flex={1} justifyContent="flex-end" marginTop="$4">
-            <Text color="$gray100" fontSize="$sm" mb="$3" fontFamily="$body">
-              Ainda não tem acesso?
-            </Text>
+        <Center mt={24}>
+          <Text color="$gray100" fontSize="$sm" mb="$3" fontFamily="$body">
+            Ainda não tem acesso?
+          </Text>
 
-            <Button title="Criar conta" variant="outline" onPress={handleNewAccount} />
-          </Center>
-        </VStack>
+          <Button 
+            title="Criar Conta" 
+            variant="outline"
+            onPress={handleNewAccount}
+          />
+        </Center>
       </VStack>
     </ScrollView>
-  )
+  );
 }
