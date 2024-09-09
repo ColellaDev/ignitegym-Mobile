a3723789eba5dc68df72ff2644fc8307b7685cba
Marcos Colella <colella.dev@gmail.com>
2024-09-09 18:16:42 -0300

    Exibindo a foto padrão do usuário

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,6 +1,7 @@
 import { Heading, HStack, Icon, Text, VStack } from "@gluestack-ui/themed";
 import { UserPhoto } from './UserPhoto'
 import { LogOut } from 'lucide-react-native'
+import defaulUserPhotoImg from '@assets/userPhotoDefault.png'; 
 
 import { useAuth } from '@hooks/useAuth';
 
@@ -10,7 +11,7 @@ export function HomeHeader() {
 
     return (
         <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap='$4'>
-            <UserPhoto source={{ uri: 'https://github.com/ColellaDev.png'}} w='$16' h='$16' alt='Imagem do usuário' />
+            <UserPhoto source={user.avatar  ? { uri: user.avatar } : defaulUserPhotoImg} w='$16' h='$16' alt='Imagem do usuário' />
             <VStack flex={1}>
                 <Text color="$gray100" fontSize="$sm">
                     Olá
