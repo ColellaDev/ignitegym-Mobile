31b70ed782193efdd6956820ac7f4bcd1f8a0bc9
Marcos Colella <colella.dev@gmail.com>
2024-09-10 09:03:35 -0300

    Criando o Exercise DTO

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -6,6 +6,7 @@ import { AppNavigatorRoutesProps } from '@routes/app.routes'
 
 import { api } from '@services/api';
 import { AppError } from '@utils/AppError';
+import { ExerciseDTO } from '@dtos/ExerciseDTO';
 
 import { HomeHeader } from '@components/HomeHeader'
 import { Group } from '@components/Group'
@@ -13,7 +14,7 @@ import { ExerciseCard } from '@components/ExerciseCard'
 
 export function Home() {
   const [groups, setGroups] = useState<string[]>([]);
-  const [exercises, setExercises] = useState([]);
+  const [exercises, setExercises] = useState<ExerciseDTO[]>([]);
   const [groupSelected, setGroupSelected] = useState('Costas');
 
   const navigation = useNavigation<AppNavigatorRoutesProps>()
@@ -45,7 +46,7 @@ export function Home() {
   async function fecthExercisesByGroup() {
     try {
       const response = await api.get(`/exercises/bygroup/${groupSelected}`);
-      console.log(response.data);
+      setExercises(response.data);
 
     } catch (error) {
       const isAppError = error instanceof AppError;
@@ -103,7 +104,7 @@ export function Home() {
 
         <FlatList
           data={exercises}
-          keyExtractor={(item) => item}
+          keyExtractor={(item) => item.id}
           renderItem={({ item }) => <ExerciseCard onPress={handleOpenExerciseDetails}/>}
           showsVerticalScrollIndicator={false}
           contentContainerStyle={{ paddingBottom: 20 }}
