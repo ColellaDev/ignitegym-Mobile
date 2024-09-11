eee88a5484b87cb6dd9e83cfe1e76eb97c361d33
Marcos Colella <colella.dev@gmail.com>
2024-09-11 11:04:17 -0300

    Refatorando e Armazenando o Refresh Token

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,14 @@
-import axios, { AxiosInstance } from "axios";
+import axios, { AxiosError, AxiosInstance } from "axios";
 
 import { AppError } from "@utils/AppError";
+import { storageAuthTokenGet } from "@storage/storageAuthToken";
 
 type SignOut = () => void;
+
+type PromiseType = {
+  onSuccess: (token: string) => void;
+  onFailure: (error: AxiosError) => void;
+}
 
 type APIInstanceProps = AxiosInstance & {
   registerInterceptTokenManager: (signOut: SignOut) => () => void;
@@ -12,16 +18,23 @@ const api = axios.create({
   baseURL: 'http://localhost:3333',
 }) as APIInstanceProps;
 
+let failedQueued: Array<PromiseType> = [];
 
 api.registerInterceptTokenManager = singOut => {
-  const interceptTokenManager = api.interceptors.response.use((response) => response, requestError => {
+  const interceptTokenManager = api.interceptors.response.use((response) => response, async (requestError) => {
     if(requestError.response?.status === 401) {
       if(requestError.response.data?.message === 'token.expired' || requestError.response.data?.message === 'token.invalid') {
+        const { refresh_token } = await storageAuthTokenGet();
 
+        if(!refresh_token) {
+          singOut();
+          return Promise.reject(requestError)
+        }
+        
       }
-
+      
       singOut();
-
+      
     }
 
     if(requestError.response && requestError.response.data) {
@@ -35,5 +48,7 @@ api.registerInterceptTokenManager = singOut => {
     api.interceptors.response.eject(interceptTokenManager);
   }
 }
+
+
 
 export { api };
